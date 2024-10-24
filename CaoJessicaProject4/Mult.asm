e00a19605dd7061253ddfc940079f5efb9eadc70
Jessica Cao <jcao3@linux3.cs.uchicago.edu>
2024-10-24 13:37:10 -0500

    Fix

--- CaoJessicaProject4/Mult.asm
+++ CaoJessicaProject4/Mult.asm
@@ -2,17 +2,17 @@
 @i // i refers to some mem. location.
 M=1 // i=1
 @2 // product refers to mem. location 2.
-M=0 // sum=0
+M=0 // prod=0
 (LOOP)
 @i
 D=M // D=i
 @0
 D=D-M // D=i-R0
 @END
-D;JGT // If (i-100)>0 goto END
+D;JGT // If (i-R0)>0 goto END
 @1
 D=M // D=R1
-@prod
+@2
 M=D+M // prod=prod+R1
 @i
 M=M+1 // i=i+1
